8dde648c702f41c3342ba2bd01731fc572d8f0ba
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-07-20 19:13:32 -0300

    componentes children

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,18 @@
 import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
-import { Container } from './styles.js'
+import { Section } from '../../components/Section'
+import { Container, Links } from './styles.js'
 export function Details() {
   return (
     <Container>
       <Header />
+<Section title="Links úteis">
+<Links>
+  <li><a href="#">https://github.com/williangomesdev</a></li>
+  <li><a href="#">https://github.com/williangomesdev</a></li>
+</Links>
 
+</Section>
       <Button title="Voltar" />
     </Container>
   )
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -4,9 +4,19 @@ export const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: grid;
-  grid-template-rows: 6.5625rem auto ;
-  grid-template-areas: 
-  "header"
-  "content"
-  ;
+  grid-template-rows: 6.5625rem auto;
+  grid-template-areas:
+    "header"
+    "content";
+`;
+
+export const Links = styled.ul`
+  list-style-type: style none;
+  > li {
+    margin-top: 0.75rem;
+
+    a {
+      color: ${({ theme }) => theme.COLORS.WHITE};
+    }
+  }
 `;
