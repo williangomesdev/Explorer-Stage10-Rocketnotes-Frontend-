d4d6ca7fe68d73cac49e44f75477b18ddc183d11
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-07-20 17:05:02 -0300

    estilização global

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
-
+import GlobalStyles from './styles/global'
 import theme from "./styles/theme"
 import { Details } from './pages/Details'
 
@@ -9,7 +9,8 @@ import { Details } from './pages/Details'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <Details />
-    </ThemeProvider>
-  </React.StrictMode>
+  </ThemeProvider>
+  </React.StrictMode >
 )
