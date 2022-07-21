723384836c71b5a64c98a913c4cb233a545ec14c
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-07-21 18:14:59 -0300

    logotipo

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -16,21 +16,26 @@ export const Container = styled.div`
 `;
 export const Brand = styled.div`
   grid-area: brand;
-  background-color: red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 0.0625rem solid ${({ theme }) => theme.COLORS.BACKGROUND_700};
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+  > h1 {
+    font-size: 1.5rem;
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+  }
 `;
 export const Menu = styled.ul`
   grid-area: menu;
-  background-color: green;
 `;
 export const Search = styled.div`
   grid-area: search;
-  background-color: blue;
 `;
 export const Content = styled.div`
   grid-area: content;
-  background-color: purple;
 `;
 export const NewNote = styled.button`
   grid-area: newnote;
-  background-color: yellow;
 `;
