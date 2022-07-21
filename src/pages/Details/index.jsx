b102b9fcf07ae0accfd8983b4b287eea55429b61
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-07-21 15:43:16 -0300

    terminando detalhes

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,25 +3,37 @@ import { ButtonText } from '../../components/ButtonText'
 import { Header } from '../../components/Header'
 import { Section } from '../../components/Section'
 import { Tag } from '../../components/Tag'
-import { Container, Links } from './styles.js'
+import { Container, Links, Content } from './styles.js'
 export function Details() {
   return (
     <Container>
       <Header />
-      <ButtonText title="Excluir nota"/>
-      <Section title="Links úteis">
-        <Links>
-          <li><a href="#">https://github.com/williangomesdev</a></li>
-          <li><a href="#">https://github.com/williangomesdev</a></li>
-        </Links>
-      </Section>
 
-      <Section title="Marcadores">
-        <Tag title="express" />
-        <Tag title="nodejs" />
-      </Section>
-      <Button title="Voltar" />
-    </Container>
+      <main>
+        <Content>
+
+          <ButtonText title="Excluir nota" />
+
+          <h1>Introdução ao React</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Magni tempore a excepturi illum illo quaerat vel deleniti assumenda veniam animi. 
+            Voluptates impedit accusantium distinctio sequi nostrum fugiat aliquid cumque totam.</p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li><a href="#">https://github.com/williangomesdev</a></li>
+              <li><a href="#">https://github.com/williangomesdev</a></li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="nodejs" />
+          </Section>
+          <Button title="Voltar" />
+        </Content>
+      </main>
+    </Container >
   )
 
 }
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -8,6 +8,12 @@ export const Container = styled.div`
   grid-template-areas:
     "header"
     "content";
+
+  > main {
+    grid-area: "content";
+    overflow-y: scroll;
+    padding: 4rem 0;
+  }
 `;
 
 export const Links = styled.ul`
@@ -18,5 +24,28 @@ export const Links = styled.ul`
     a {
       color: ${({ theme }) => theme.COLORS.WHITE};
     }
+  }
+`;
+
+export const Content = styled.div`
+  max-width: 34.375rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+
+  > button:first-child {
+    align-self: end;
+  }
+
+  > h1 {
+    font-size: 2.25rem;
+    font-weight: 500;
+    padding-top: 4rem;
+  }
+
+  > p {
+    font-size: 1rem;
+    margin-top: 1rem;
+    text-align: justify;
   }
 `;
