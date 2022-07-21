7808301623bf0ff6bf292b96b035ee185b676a52
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-07-21 14:33:59 -0300

    componente tag

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,18 +1,23 @@
 import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
 import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
 import { Container, Links } from './styles.js'
 export function Details() {
   return (
     <Container>
       <Header />
-<Section title="Links úteis">
-<Links>
-  <li><a href="#">https://github.com/williangomesdev</a></li>
-  <li><a href="#">https://github.com/williangomesdev</a></li>
-</Links>
+      <Section title="Links úteis">
+        <Links>
+          <li><a href="#">https://github.com/williangomesdev</a></li>
+          <li><a href="#">https://github.com/williangomesdev</a></li>
+        </Links>
+      </Section>
 
-</Section>
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="nodejs" />
+      </Section>
       <Button title="Voltar" />
     </Container>
   )
