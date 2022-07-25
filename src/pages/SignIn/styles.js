fdeb16e949ea197ecb926fdb218249762aced2ca
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-07-25 17:39:22 -0300

    formulário de login

--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -1,7 +1,36 @@
-import styled from "styled-components";
+import styled, { ThemeContext } from "styled-components";
 
 export const Container = styled.div`
   height: 100vh;
   display: flex;
   align-items: stretch;
+`;
+export const Form = styled.form`
+  padding: 0 8.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  > h1 {
+    font-size: 3rem;
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+  }
+
+  > h2 {
+    font-size: 1.5rem;
+    margin-top: 5.25rem;
+    margin-bottom: 1.5rem;
+  }
+
+  > p {
+    font-size: 14px;
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+  }
+
+  > a {
+    margin-top: 7.75rem;
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+  }
 `;
