f456b8397d9fa67e3f9e3695c52f0451ee01cf74
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-08-28 11:43:07 -0300

    Cadastro de usuario API Restful

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -19,7 +19,7 @@ export const Container = styled.div`
     border: 0;
     background: transparent;
 
-    &:placeholder {
+    &::placeholder {
       color: ${({ theme }) => theme.COLORS.GRAY_300};
     }
   }
