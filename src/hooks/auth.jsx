59cd9bc10aac1e2b4deb69339e8622fe6925646e
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-08-30 11:08:05 -0300

    Redirecionamento de rotas

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -30,7 +30,7 @@ function AuthProvider({ children }) {
 
 
   return (
-    <AuthContext.Provider value={{ signIn }}>
+    <AuthContext.Provider value={{ signIn, user:data.user }}>
       {children}
     </AuthContext.Provider>
   );
